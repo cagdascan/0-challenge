a1adaea7b292eadd895abc85131d35f69b97368b
Cagdas Can <cagdascan@gmail.com>
2017-11-11 15:40:44 +0300

    added google map api key

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -7,9 +7,13 @@ import {
   Marker,
 } from 'react-google-maps';
 
+import { GOOGLE_MAPS_API_KEY } from '../../config';
+
+const API_KEY = GOOGLE_MAPS_API_KEY;
+
 const Map = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp',
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${API_KEY}`,
     loadingElement: <div style={{ height: '100%' }} />,
     containerElement: <div style={{ height: 'calc(100vh - 64px)' }} />,
     mapElement: <div style={{ height: '100%' }} />,
